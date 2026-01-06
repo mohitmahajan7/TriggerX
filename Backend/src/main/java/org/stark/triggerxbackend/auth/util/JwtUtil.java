38da622ea57dcec5ec05981b4d246e4cb7124369
Mohit Mahajan <postboxcoder@gmail.com>
2026-01-06 22:49:29 +0530

    Added Jwt Util

---
+++ Backend/src/main/java/org/stark/triggerxbackend/auth/util/JwtUtil.java
@@ -0,0 +1,34 @@
+package org.stark.triggerxbackend.auth.util;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class JwtUtil {
+
+    private static final String SECRET_KEY = "triggerx123";
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token, String email) {
+        return extractEmail(token).equals(email);
+    }
+}
